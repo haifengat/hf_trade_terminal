f1e23dfce2ded2d3f6b3de91a996925a48385107
haifeng <hubert28@qq.com>
2018-07-12 20:16:59 +0800

    更新:采用新版20180109版CTP接口

---
+++ README.md
@@ -0,0 +1,17 @@
+# hf_trade_terminal
+交易终端
+## 功能
+* 行情显示
+* 下单
+	* 跟盘价/指定价
+	* 自动开平
+	* 下单偏移
+* 交易数据显示
+	* 权益
+	* 持仓
+	* 委托
+	* 成交
+	* 合约信息
+	
+## 界面
+![](http://files.git.oschina.net/group1/M00/00/D0/PaAvDFh9g2SAHNb5AABapPBwMeE675.png?token=b10bb65f90a261462f4134155552b40b&ts=1484620597&attname=terminal.png)
---
+++ Terminal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaiFeng
+{
+	partial class HFForm
+	{
+
+		void LogFinish()
+		{
+			if (File.Exists("沪深A股.txt"))
+			{
+				foreach (var line in (File.ReadAllLines("沪深A股.txt")))
+				{
+					if (char.IsDigit(line[0]))
+					//this.comboBoxInstrument.Items.Add(line.Split('\t')[0]);
+					{
+						_t.DicInstrumentField[line.Split('\t')[0]] = new InstrumentField
+						{
+							InstrumentID = line.Split('\t')[0],
+							//ExchangeID = "sh",							
+							PriceTick = 0.01,
+							ProductClass = ProductClassType.SpotOption,
+							VolumeMultiple = 1,
+							MaxOrderVolume = 1000000000,
+							//ProductID
+						};
+					}
+				}
+				this.comboBoxInstrument.Items.AddRange(_t.DicInstrumentField.Keys.ToArray());
+			}
+		}
+	}
+}
